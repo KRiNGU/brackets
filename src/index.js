3f661bc2c80129635b91dabf2812976b8c32e772
LEGION <vip.489@inbox.ru>
2021-03-09 16:43:41 +0300

    feat: created cheack function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] == bracketsConfig[j][0]) {
+        if (bracketsConfig[j][0] == bracketsConfig[j][1] && 
+          stack[stack.length - 1] == bracketsConfig[j][0]) {
+          stack.pop();
+        }
+        else {
+          stack.push(bracketsConfig[j][0]);
+        }
+      }
+      else if (str[i] == bracketsConfig[j][1]) {
+        if (stack[stack.length - 1] != bracketsConfig[j][0]) {
+          return false;
+        }
+        stack.pop();
+      }
+    }
+  }
+  return stack.length == 0;
 }
